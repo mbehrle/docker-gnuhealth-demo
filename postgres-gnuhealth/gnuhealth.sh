dd54666faeeb3bc849b322ec6496607ea862e34a
Mathias Behrle <mathiasb@m9s.biz>
2016-03-25 16:27:24 +0100

    Updating to the changed initdb logic of the postgres container.
    - The new initdb logic of the postgres container broke the --single
      setup of our container. As the --single switch is deprecated by the
      postgres folks, this is the cleaner way anyway.
    - Simplifying the check for an existing gnuhealth database.
    
    Refs:
    - https://github.com/mbehrle/docker-gnuhealth-demo/issues/2
    - https://github.com/docker-library/postgres/pull/75
    - https://github.com/docker-library/postgres/issues/78

--- postgres-gnuhealth/gnuhealth.sh
+++ postgres-gnuhealth/gnuhealth.sh
@@ -11,24 +11,30 @@ fi
 : ${DB_ENCODING:=UTF-8}
 : ${DB_DUMPFILE:=/demo.sql.gz}
 
+# Perform all actions as user 'postgres'
+export PGUSER=postgres
+
 # Test if the gnuhealth database already exists
-INIT=$(gosu postgres postgres --single -j <<-EOSQL
+echo "Checking for existing database..."
+INIT=$(psql -d template1 -t <<-EOSQL
         SELECT COUNT(*) from pg_database where datname = '$GNUHEALTH_DB';
 EOSQL
 )
-if [ `expr "${INIT}" : '.*count = "\([0-9]\).*'` == "0" ]; then
+INIT="$(echo "$INIT" | sed -e 's/^[ \t]*//;s/[ \t]*$//')"
+
+if [ "${INIT}" == "0" ]; then
 	echo "Creating the gnuhealth role..."
-	gosu postgres postgres --single -jE <<-EOSQL
+	psql <<-EOSQL
 		CREATE USER $GNUHEALTH_USER WITH NOSUPERUSER NOINHERIT CREATEDB NOCREATEROLE NOREPLICATION $PASS;
 	EOSQL
 	echo
 	echo "Creating the gnuhealth database..."
-	gosu postgres postgres --single -jE <<-EOSQL
+	psql <<-EOSQL
 		CREATE DATABASE $GNUHEALTH_DB WITH OWNER $GNUHEALTH_USER ENCODING='$DB_ENCODING';
 	EOSQL
 	echo
 	echo "Importing the gnuhealth database..."
-	{ gosu postgres pg_ctl start -w && gosu postgres gunzip -c "$DB_DUMPFILE" | psql -U postgres -d "$GNUHEALTH_DB" && gosu postgres pg_ctl stop -w; }
+	{ gosu postgres gunzip -c "$DB_DUMPFILE" | psql -d "$GNUHEALTH_DB"; }
 	echo
 	echo "Allowing access for the gnuhealth user from all IPs..."
 	{ echo; echo "host all $GNUHEALTH_USER 0.0.0.0/0 md5"; } >> "$PGDATA"/pg_hba.conf
